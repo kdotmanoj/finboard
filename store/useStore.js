508a3cf0cc18317182662510b4179ad106edd6f8
kdotmanoj <hsuper681@gmail.com>
2025-12-26 01:18:54 +0530

    Setup global store and data mapping utils

--- store/useStore.js
+++ store/useStore.js
@@ -2,26 +2,31 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 const useStore = create(
-  persist(
-    (set) => ({
-      widgets: [],
+    persist(
+        (set) => ({
+        widgets: [],
 
-      addWidget: (newWidget) => set((state) => ({
-        widgets: [...state.widgets, newWidget]
-      })),
+        addWidget: (newWidget) => set((state) => {
+            const id = Date.now().toString();
+            const newLayoutItem = { i: id, x: 0, y: Infinity, w: 4, h: 2 };
+            
+            return {
+            widgets: [...state.widgets, { ...widget, id }],
+            layout: [...state.layout, newLayoutItem]
+            };
+        }),
 
-      removeWidget: (widgetId) => set((state) => ({
-        widgets: state.widgets.filter((w) => w.id !== widgetId)
-      })),
+        removeWidget: (id) => set((state) => ({
+            widgets: state.widgets.filter((w) => w.id !== id),
+            layout: state.layout.filter((l) => l.i !== id)
+        })),
 
-      updateLayout: (newLayout) => set((state) => {
-        return { layout: newLayout };
-      }),
-    }),
-    {
-      name: 'finboard-storage',
-    }
-  )
+        updateLayout: (newLayout) => set({ layout: newLayout }),
+        }),
+        {
+        name: 'finboard-storage',
+        }
+    )
 );
 
 export default useStore;
---
+++ utils/helpers.js
@@ -0,0 +1,18 @@
+export const getNestedValue = (obj,path) => {
+    if(!obj || !path) return undefined;
+
+    return path.split('.').reduce((acc,part) => {
+        return acc & acc[part] !== undefined ? acc[part] : null;
+    },obj);
+};
+
+export const flattenObject = (obj, prefix = '') => {
+  return Object.keys(obj).reduce((acc, key) => {
+    const path = prefix ? `${prefix}.${key}` : key;
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      return { ...acc, ...flattenObject(obj[key], path) };
+    } else {
+      return { ...acc, [path]: obj[key] };
+    }
+  }, {});
+};
